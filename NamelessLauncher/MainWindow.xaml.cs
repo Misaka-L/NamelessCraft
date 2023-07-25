9d2b847f31427c65e208709bd53cde71ae3490bf
Misaka-L <lipww1234@foxmail.com>
2023-07-25 17:07:53 +0800

    feature: launcher itself (microsoft authentication text)

---
+++ NamelessLauncher/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Identity.Client;
+using NamelessCraft.Authenticator;
+
+namespace NamelessLauncher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IPublicClientApplication _clientApp = PublicClientApplicationBuilder
+            .Create("41da5906-0a4e-4385-ac40-711d4214eddd")
+            .WithRedirectUri("http://localhost:46187")
+            .WithAuthority(AadAuthorityAudience.PersonalMicrosoftAccount)
+            .Build();
+        
+        private string graphAPIEndpoint = "https://graph.microsoft.com/v1.0/me";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            AuthenticationResult authResult = null;
+            var app = _clientApp;
+            ResultText.Text = string.Empty;
+            TokenInfoText.Text = string.Empty;
+
+            var accounts = await app.GetAccountsAsync();
+            var firstAccount = accounts.FirstOrDefault();
+
+            try
+            {
+                authResult = await app.AcquireTokenSilent(new[] { "XboxLive.signin" }, firstAccount)
+                    .ExecuteAsync();
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                // A MsalUiRequiredException happened on AcquireTokenSilent.
+                // This indicates you need to call AcquireTokenInteractive to acquire a token
+                System.Diagnostics.Debug.WriteLine($"MsalUiRequiredException: {ex.Message}");
+
+                try
+                {
+                    authResult = await app.AcquireTokenInteractive(new[] { "XboxLive.signin" })
+                        .WithAccount(accounts.FirstOrDefault())
+                        .WithPrompt(Prompt.SelectAccount)
+                        .WithUseEmbeddedWebView(false)
+                        .ExecuteAsync();
+                }
+                catch (MsalException msalex)
+                {
+                    ResultText.Text = $"Error Acquiring Token:{System.Environment.NewLine}{msalex}";
+                }
+            }
+            catch (Exception ex)
+            {
+                ResultText.Text = $"Error Acquiring Token Silently:{System.Environment.NewLine}{ex}";
+                return;
+            }
+
+            if (authResult != null)
+            {
+                var result = await new MicrosoftAuthenticator(options =>
+                {
+                    options.MicrosoftAccountAccessToken = authResult.AccessToken;
+                }).AuthenticateAsync();
+
+                ResultText.Text = result.ToString();
+                DisplayBasicTokenInfo(authResult);
+            }
+        }
+        
+        /// <summary>
+        /// Display basic information contained in the token
+        /// </summary>
+        private void DisplayBasicTokenInfo(AuthenticationResult authResult)
+        {
+            TokenInfoText.Text = "";
+            if (authResult != null)
+            {
+                TokenInfoText.Text += $"Username: {authResult.Account.Username}" + Environment.NewLine;
+                TokenInfoText.Text += $"Token Expires: {authResult.ExpiresOn.ToLocalTime()}" + Environment.NewLine;
+            }
+        }
+    }
+}
